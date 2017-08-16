e14005b9de8968c2a565ec419bcd23213900e346
Min Kyu Choi <minkyuchoi999@gmail.com>
2017-08-15 19:27:20 -0700

    remove TODOs from the file since homework is done

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -13,14 +13,6 @@ namespace UrlShortener
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // TODO: add a routing rule that matches shortUrl of a Url object, and then redirects users to longUrl
-            //       hint, use RedirectPermanent in a controller
-            //       localhost/shortUrl -> redirect to longUrl
-            // TODO: do not allow users to enter a date from views, instead auto generate when a row is inserted
-            // TODO: remove all unnecessary views from your Views folder
-            // TODO: modify the layout of all the views (remember _Layout & Shared)
-            // TODO: make views colorful (make them pretty regardless of your workload)
-
             routes.MapRoute(
                 name: "URL",
                 url: "{shortURL}",
